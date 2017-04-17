b915bc6d37331240cf32820ce8e72ea745989e06
SergM2014 <weisse011@gmail.com>
2017-04-17 08:56:30 +0300

    admin link in footer added

--- resources/templates/default/footer.php
+++ resources/templates/default/footer.php
@@ -2,6 +2,10 @@
             <?php if(@!$noTemplate): ?>
                 <footer class="site-footer">
                     <span class="site-footer__mark"><?= $footerDisclaimerL ?></span>
+
+                        <a href="/admin" class="site-footer__admin-link"><small>admin</small></a>
+
+
                 </footer>
 
                 <script src="<?= ROOT ?>/assets/js/script.js?ver=<?= time() ?>" ></script>
